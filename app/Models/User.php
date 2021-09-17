2107b0b6c2c3a55a5941dfd0a1d63214813f7bb2
edgpin16 <edgardo.pinto16@gmail.com>
2021-09-16 22:29:58 -0400

    Agregando el paquete laravel permissions de Spatie

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
 
 use App\Models\Pharmacy;
 use App\Models\Laboratory;
@@ -15,6 +16,8 @@ use App\Models\Subsidiary;
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
+
+    use HasRoles;
 
     /**
      * The attributes that are mass assignable.
